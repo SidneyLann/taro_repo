bce6b49c91a1b26cd8e861a1fc5255ce81033096
Administrator <Administrator@tech8>
2024-10-21 15:03:34 +0800

    jj

---
+++ taro_test/.linaria/custom.js
@@ -0,0 +1,1 @@
+import {css,styled} from './react.js';
--- taro_test/babel.config.js
+++ taro_test/babel.config.js
@@ -1,16 +1,20 @@
-// babel-preset-taro 更多选项和默认值：
-// https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
   presets: [
     ['taro', {
       framework: 'react',
       ts: true,
       compiler: 'webpack5',
-    }]
+    }],
+	//'module:metro-react-native-babel-preset',
+	//'module:@react-native/babel-preset',
+	'@wyw-in-js/babel-preset',
+	//'@wyw-in-js',
+	//'@linaria'
   ],
   plugins: [
    ["@babel/plugin-proposal-decorators", { "version": "legacy" }],
-   ["@babel/plugin-transform-class-properties", { "loose": true }],
+   //["@babel/plugin-transform-class-properties", { "loose": true }],
    ["@babel/plugin-transform-private-methods", {"loose": true}],
+   //["@babel/plugin-transform-react-jsx", {"runtime": "automatic"}]
   ]
 }
